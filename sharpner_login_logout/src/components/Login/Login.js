ba26b226c8313dbac25b42cea966b94c50571560
Drcodepro <drrahangdale01@gmail.com>
2024-03-04 11:56:08 +0530

    implemented Debouncing using setTimeOut() and clearTimeOut() function inside the useEffet()...

--- sharpner_login_logout/src/components/Login/Login.js
+++ sharpner_login_logout/src/components/Login/Login.js
@@ -14,10 +14,19 @@ const Login = (props) => {
 
   const [formIsValid, setFormIsValid] = useState(false);
 
+  // we want that we will not check the validation in every keystrock instead of this we want to check validation when the user stops the typing this will encrease the performance of app so ...
+  // we use settimeout() function that will wait for a 500 sec. to executes butt this will always triger when user type so we want only 1 time this timeout function will run so here we use the Debouncing and clean up using  clearTimeOut() function
   useEffect(()=>{
-    setFormIsValid(
+    const timerValid = setTimeout(()=>{ // this will run only very first time after this will not run
+      setFormIsValid(
       enteredEmail.includes('@') && enteredPassword.trim().length > 6 && enteredCollage.trim().length > 3
-    );
+    )},500)
+
+    // this is like cleanup function
+    return ()=>{   // this function runs before the above function execution instead of first time only first time above code will execute before this returned function 
+      clearTimeout(timerValid);// and this function clears all the previous setTimeOut() function so only one timeout will work
+    }
+    
   },[enteredEmail,enteredPassword,enteredCollage]) //if one of thise dependency changes only when this useEffect will trigur okkk...
 
   const emailChangeHandler = (event) => {
